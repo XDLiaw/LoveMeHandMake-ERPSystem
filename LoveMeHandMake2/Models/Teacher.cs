973cbb9b970aa135ef6994dd130326a38cad0ec0
Eric Liao <eric855104@gmail.com>
2016-03-09 14:24:47 +0800

    Model Teacher.cs 新增離職日期欄位 並在每個欄位加上Display Name

--- LoveMeHandMake2/Models/Teacher.cs
+++ LoveMeHandMake2/Models/Teacher.cs
@@ -10,42 +10,60 @@ namespace LoveMeHandMake2.Models
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "不能为空")]
         [StringLength(50)]
+        [Display(Name = "姓名")]
         public string Name { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "不能为空")]
+        [Display(Name = "帐号/员工编号")]
         public string AccountID { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "不能为空")]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "密码长度必须在{2} 和{1}之间")]
+        [Display(Name = "密码")]
         public string Password { get; set; }
 
         [StringLength(20, MinimumLength = 6, ErrorMessage = "密码长度必须在{2} 和{1}之间")]
+        [Display(Name = "旧密码")]
         public string PreviousPassword { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "不能为空")]
+        [Display(Name = "是否可发公关卡")]
         public bool CanPublishPRCard { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "不能为空")]
         [Phone]
+        [Display(Name = "电话")]
         public string Phone { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "不能为空")]
         public int BelongStoreID { get; set; }
 
+        [Display(Name = "所属门市")]
+        public virtual Store BelongStore { get; set; }
+
         [Required(AllowEmptyStrings = false, ErrorMessage = "不能为空")]
+        [Display(Name = "到职日")]
         public DateTime ArriveDate { get; set; }
 
+        [Display(Name = "离职日")]
+        public DateTime ResignDate { get; set; }
+
         [Required(AllowEmptyStrings = false, ErrorMessage = "不能为空")]
+        [Display(Name = "生日")]
         public DateTime Birthday { get; set; }
 
+        [Display(Name = "籍贯")]
         public string Birthplace { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "不能为空")]
+        [Display(Name = "身分证号")]
         public string IDNumber { get; set; }
 
         [EmailAddressAttribute]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [Display(Name = "地址")]
         public string Address { get; set; }
     }
 }
