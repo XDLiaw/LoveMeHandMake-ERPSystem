61b9d481f00e84c6556a1ae34c542eac9fc3656a
Eric Liao <eric855104@gmail.com>
2016-03-09 11:54:03 +0800

    新增"系統參數設定" View and Controller

---
+++ LoveMeHandMake2/Controllers/SysParameterController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LoveMeHandMake2.Models;
+
+namespace LoveMeHandMake2.Controllers
+{
+    public class SysParameterController : Controller
+    {
+        private LoveMeHandMakeContext db = new LoveMeHandMakeContext();
+
+        // GET: SysParameter
+        public ActionResult Index()
+        {
+            return View(db.SysParameter.ToList());
+        }
+
+        // GET: SysParameter/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SysParameter sysParameter = db.SysParameter.Find(id);
+            if (sysParameter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sysParameter);
+        }
+
+        // POST: SysParameter/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(SysParameter sysParameter)
+        {
+            if (ModelState.IsValid)
+            {
+                sysParameter.Update();
+                db.Entry(sysParameter).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sysParameter);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- LoveMeHandMake2/Models/SysParameter.cs
+++ LoveMeHandMake2/Models/SysParameter.cs
@@ -9,11 +9,14 @@ namespace LoveMeHandMake2.Models
     public class SysParameter : BaseModel
     {
         [Required]
+        [Display(Name = "参数名称")]
         public string Key { get; set; }
 
         [Required]
+        [Display(Name = "参数值")]
         public string Value { get; set; }
 
+        [Display(Name = "说明")]
         public string Note { get; set; }
     }
 }
